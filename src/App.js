ae838f30fb096f0bdcc1a214c49171a8e5ce864e
Alexander Byun <alexanderbyun@AB-MBP16.attlocal.net>
2022-07-04 19:19:11 -0400

    remove typescript. fresh setup of react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+
+function App() {
+  return (
+    <>
+     <h1>Hello World!</h1>
+    </>
+  );
+}
+
+export default App;
